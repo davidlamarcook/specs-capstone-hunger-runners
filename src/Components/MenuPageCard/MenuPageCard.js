9a0556575c78e502af420360662d4145b27ba44d
davidlamarcook <dcook1025@gmail.com>
2022-11-19 21:41:46 -0700

    added styling to menu page cards

--- src/Components/MenuPageCard/MenuPageCard.js
+++ src/Components/MenuPageCard/MenuPageCard.js
@@ -1,13 +1,21 @@
 import React from "react";
+import './MenuPageCard.css'
 
 function MenuPageCard(props) {
 
     console.log(props)
     return (
-        <>
-            <h1>{props.menuItemList.food_item_name}</h1>
-            <h1>{props.menuItemList.food_item_price}</h1>
-        </>
+        <div id="menuCardContainer">
+            <style>{'.App {display: flex; flex-wrap: wrap; gap: 15px; justify-content: center; align-items: center;}'}</style>
+            <div>
+                <img src={`${process.env.PUBLIC_URL}/images/${props.menuItemList.food_item_picture}`} id="mainPageCardImg"/>
+            </div>
+            <div id="menuCardContent">
+                <h1>{props.menuItemList.food_item_name}</h1>
+                <h2 id="menuCardContent-h2">${props.menuItemList.food_item_price}</h2>
+                <button id="addToMenuBtn">+</button>
+            </div>
+        </div>
     )
 }
 
